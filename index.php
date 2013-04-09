c127f7f11a19332677a12ae521b1afbaed99364f
Howard Chung <howardc93@gmail.com>
2013-04-09 06:35:27 -0400

    test

--- index.php
+++ index.php
@@ -138,10 +138,10 @@ for($i = 0; $i < count($friendNames); $i++)
       <div class="navbar-inner">
         <div class="container">
           <a href"/" class="brand">Facebook Friend Name Analyzer, by Howard Chung</a>
-                </div>
-       <ul class="nav pull-right">
+                 <ul class="nav pull-right">
                 <div class="fb-like" data-href="http://nameanalyzer.herokuapp.com" data-send="false" data-width="450" data-show-faces="true"></div>
             </ul>
+                </div>
         </div>
     </div>
 
